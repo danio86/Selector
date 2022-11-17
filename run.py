a6bf9b35dc5a45733f73a2450ba723275dfe6c98
danio86 <d.harder@gmx.at>
2022-11-17 10:13:36 +0000

    Initial commit + get url with Beautiful Soup

--- run.py
+++ run.py
@@ -1,3 +1,18 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+from bs4 import BeautifulSoup 
+import requests
+
+#url_series = 'https://www.imdb.com/chart/toptv/?ref_=nv_tvv_250'
+url_movies = 'https://www.imdb.com/chart/top/?ref_=nv_mv_250'
+#url_recipes = 'https://www.tasteofhome.com/collection/our-100-highest-rated-recipes-ever/'
+
+def main():
+    request = requests.get(url_movies)
+    html = request.text
+
+    soup = BeautifulSoup(html, 'html.parser')
+    #print(soup)
+    characteristics = soup.select('td.titleColumn')
+    print(characteristics)
+
+main()
