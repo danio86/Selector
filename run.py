e4c454bce1a30280ac464de9e7387bbac30933c6
danio86 <d.harder@gmx.at>
2022-11-18 16:50:27 +0000

    add input function incl try-except

--- run.py
+++ run.py
@@ -1,23 +1,44 @@
 import random
-""" from bs4 import BeautifulSoup 
-import requests """
 import csv
+import re
 
-#url_series = 'https://www.imdb.com/chart/toptv/?ref_=nv_tvv_250'
-#url_movies = 'https://www.imdb.com/chart/top/?ref_=nv_mv_250'
-#url_recipes = 'https://www.tasteofhome.com/collection/our-100-highest-rated-recipes-ever/'
+
+def get_selection():
+    """
+    Get choice import from user
+    """
+    while True:
+        print('What do you want to watch?')
+        print('Enter r for random choice or p for pre-selection')
+
+        pre_choice = input('Enter your choice: \n')
+        # use allways \n in inputs!
+
+        accepted_inputs = ['r', 'p', 'R', 'P']
+        try:
+            if pre_choice in accepted_inputs:
+                print('Data is valid!')
+                break
+            else:
+                raise TypeError(f'Invalid data: {pre_choice}! Please try again!\n')
+        except TypeError as type_error:
+            # type_error gets a variable
+            print(type_error)
+    return True
+
+
+def pre_selection():
+    """
+    run all functions
+    """
+    print('Do you want to watch a movie or a series?')
+
 
 def main():
-    """ request = requests.get(url_movies)
-    html = request.text """
-
-    """ soup = BeautifulSoup(html, 'html.parser')
-    characteristics = soup.select('td.titleColumn')
-    print(characteristics[0])
-    print(type(characteristics))
-    movie_data = characteristics[0].text.split()
-    print(movie_data)
-    print(len(characteristics)) """
+    """
+    run all functions
+    """
+    get_selection()
 
     file = open('test_film.csv')
     rows = csv.reader(file, delimiter=',')
@@ -28,16 +49,22 @@ def main():
      
     keys = watch[0]
     values = watch[1:]
-    print(values, 'test')
-    
-    for item in values[0]:
-        val = item.split(',')
-        print(val, len(val), 'why?')
 
-    for xy in values:
-        print(xy.split(','), len(xy), 'now?')
+    val_new = []
+    for item in values:
+        #replace all ',' into ';' inbetween ""
+        new_str = re.sub(r'"[^"]+"', lambda x: x.group().replace(',', ';'), item[0])
+        val = new_str.split(',')
+        new_val = []
+        #replaces all ';' into , (after splitting!)
+        for dot_comma in val:
+            new_val.append(dot_comma.replace(';', ','))
+        val_new.append(new_val)
+    #print(val_new[0])
+
     
-  
+
+            
     
     
     
